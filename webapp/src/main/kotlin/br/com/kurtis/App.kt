b285d7348f6cf9210d3497b3dfe52cab1ebb47e8
Tiago Angelo <tiago.angelo@sensedia.com>
2019-07-16 20:46:46 -0300

    Using Javalin as REST API Library

--- webapp/src/main/kotlin/br/com/kurtis/App.kt
+++ webapp/src/main/kotlin/br/com/kurtis/App.kt
@@ -1,7 +1,19 @@
 package br.com.kurtis
 
-class App
+import br.com.kurtis.http.resultJson
+import io.javalin.Javalin
+import io.javalin.apibuilder.ApiBuilder.get
+import io.javalin.apibuilder.ApiBuilder.path
 
 fun main() {
-    println("Hello World!")
+    Javalin
+            .create { it.enableCorsForAllOrigins() }
+            .routes {
+                path("users") {
+                    get("/") { it.resultJson(Response("Hello World")) }
+                }
+            }
+            .start(8080)
 }
+
+data class Response(val message: String)
